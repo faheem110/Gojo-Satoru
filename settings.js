9ee34146873bd4e36ae2d228a4be2f9162ffe568
F@#€€m <97154709+faheem110@users.noreply.github.com>
2022-06-26 18:13:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['923048080917']
+global.premium = ['6285775549445']
+global.ownernomer = '923048080917'
+global.ownername = '🐦Fsayang ☕Phya'
 global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.footer = '©NFsayangPhyawInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Fsayang ☕Phya'
+global.author = '🐦 Fsayang phya ☕'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
